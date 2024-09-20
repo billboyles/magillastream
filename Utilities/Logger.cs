6c851103f371ef1de1b484fb502b14f1fd9be3e5
billboyles <bill.boyles@gmail.com>
2024-09-19 22:30:48 -0500

    add logger

--- Utilities/Logger.cs
+++ Utilities/Logger.cs
@@ -1,14 +1,38 @@
 using System.IO;
+using System;
 
 namespace MagillaStream.Utilities
 {
     public static class Logger
     {
-        private static readonly string logFilePath = "Logs/app_log.txt";
+        private static readonly string logFilePath = "Logs/AppLog.log";
 
-        public static void Log(string message)
+        public static void Log(string level, string message)
         {
-            File.AppendAllText(logFilePath, message + "\n");
+            // Create a log message with the current date and time
+            var logMessage = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {level}: {message}";
+
+            // Append the log message to the log file
+            File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+        }
+        
+        public static void Debug(string message)
+        {
+            Log("DEBUG", message);
+        } 
+        public static void Info(string message)
+        {
+            Log("INFO", message);
+        }
+
+        public static void Warning(string message)
+        {
+            Log("WARNING", message);
+        }
+
+        public static void Error(string message)
+        {
+            Log("ERROR", message);
         }
     }
 }
